2ba56a642e1da16dbf37116fa11ea4a55c17d271
zealptl <zealpatel06@gmail.com>
2021-09-28 12:01:42 -0400

    add histoStretch

--- hw1/HW_histoStretch.cpp
+++ hw1/HW_histoStretch.cpp
@@ -10,4 +10,25 @@ using namespace IP;
 void
 HW_histoStretch(ImagePtr I1, int t1, int t2, ImagePtr I2)
 {
+    // copy image header (width, height) of input image I1 to output image I2
+    IP_copyImageHeader(I1, I2);
+    
+    // int vars for height, width, and total number of pixels
+    int width = I1->width();
+    int height = I1->height();
+    int total = width * height;
+    int diff = t2 - t1;
+    
+    // init LUT
+    int i, LUT[MXGRAY];
+    for (i = 0; i < MXGRAY; i++) LUT[i] = CLIP((int)((MaxGray * (i - t1)) / diff), 0, MaxGray);
+    
+    // declare image channel pointer and datatype;
+    ChannelPtr<uchar> p1, p2;
+    int type;
+    
+    for (int ch=0; IP_getChannel(I1, ch, p1, type); ch++) {
+        IP_getChannel(I2, ch, p2, type);
+        for(i = 0; i < total; i++) *p2++ = LUT[*p1++];
+    }
 }
