245406034bb22a563c2d7c09aed56bb17a89dd98
Kelly Dong <dongweiling27@gmail.com>
2021-11-04 20:18:35 -0400

    updated convolve and median

--- hw2/HW_convolve.cpp
+++ hw2/HW_convolve.cpp
@@ -1,7 +1,8 @@
 #include "IP.h"
 using namespace IP;
 
-extern void copyRowToBuffer(ChannelPtr<uchar>, short*, int, int);
+//from errDiff file
+extern void copyRowToBuff(ChannelPtr<uchar>, short*, int, int);
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // HW_convolve:
 //
@@ -12,68 +13,78 @@ void
 HW_convolve(ImagePtr I1, ImagePtr Ikernel, ImagePtr I2)
 {
     int sz = Ikernel->width();
-    // if(sz % 2 == 0) sz++; // set sz to always be an odd number
 
     IP_copyImageHeader(I1, I2);
     int w = I1->width();
     int h = I1->height();
-    int total = w*h;
+    int total = w * h;
 
     int type;
     ChannelPtr<uchar> p1, p2, endd;
 
-    if (sz == 1) {
-        for(int ch = 0; IP_getChannel(I1, ch, p1, type); ch++) {
+    if(sz == 1){
+        for(int ch = 0; IP_getChannel(I1, ch, p1, type); ch++){
             IP_getChannel(I2, ch, p2, type);
-            for(endd = p1 + total; p1<endd;) *p2++ = *p1++;
+            for(endd = p1 + total; p1 < endd;){
+                *p2++ = *p1++;
+            }
         }
-    } else if (sz > 1) {
-
+    } else if(sz > 1){
         ChannelPtr<float> pKernel; // kernel value pointer
         IP_getChannel(Ikernel, 0, pKernel, type);
 
-        int bufSz = sz+w-1; // size of buffer for each padded row
+        int bufSz = sz + w - 1; // size of buffer for each padded row
         short* buffers[sz]; // array of sz pointers
-        for (int i=0; i<sz; i++) buffers[i] = new short[bufSz];
+        for(int i = 0; i < sz; i++){
+            buffers[i] = new short[bufSz];
+        }
 
-        for(int ch = 0; IP_getChannel(I1, ch, p1, type); ch++) {
+        for(int ch = 0; IP_getChannel(I1, ch, p1, type); ch++){
             IP_getChannel(I2, ch, p2, type);
-            endd = p1+total;
+            endd = p1 + total;
 
             // copy first row to first sz/2 buffers, where are top padded rows
-            for (int i=0; i<sz/2; i++) copyRowToBuffer(p1, buffers[i], w, sz);
-
-            // continue to rest of buffers, note here we still begin copying from frist row of I1
-            for (int i=sz/2; i<sz; i++) {
-                copyRowToBuffer(p1, buffers[i], w, sz);
-                if (p1 < endd-w) p1+=w;
+            for(int i = 0; i < sz / 2; i++){
+                copyRowToBuff(p1, buffers[i], w, sz);
             }
 
-            for (int y=0; y<h; y++) {  // visit each row
+            // continue to rest of buffers
+            for(int i = sz / 2; i < sz; i++){
+                copyRowToBuff(p1, buffers[i], w, sz);
+                if(p1 < endd - w){
+                    p1 += w;
+                }
+            }
+
+            for(int y = 0; y < h; y++){  // visit each row
                 // visit each pixel in row
-                for (int x=0; x<w; x++) {
+                for(int x = 0; x < w; x++){
                     float sum = 0.0;
-                    for (int i=0; i<sz; i++) {
-                        for (int j=0; j<sz; j++) {
-                          sum += (buffers[i][j+x] * *pKernel++);
+                    for(int i = 0; i < sz; i++){
+                        for(int j = 0; j < sz; j++){
+                          sum += (buffers[i][j + x] * *pKernel++);
                         }
                     }
-                    pKernel -= sz*sz;
+                    pKernel -= sz * sz;
                     *p2++ = (int)CLIP(sum, 0, 255);
                 }
 
                 // circular quque
                 // remove buffers[0] and push all other buffers up by one index
-                for (int i = 0; i<sz-1; i++) {
-                  for (int j = 0; j<bufSz; j++) {
-                    buffers[i][j] = buffers[i+1][j];
+                for(int i = 0; i < sz - 1; i++){
+                  for(int j = 0; j < bufSz; j++){
+                    buffers[i][j] = buffers[i + 1][j];
                   }
                 }
                 // copy next row to last buffer
-                copyRowToBuffer(p1, buffers[sz-1], w, sz); // copy new row to the last buffer
-                if (p1 < endd-w) p1+=w;
+                copyRowToBuff(p1, buffers[sz - 1], w, sz);
+                // copy new row to the last buffer
+                if(p1 < endd - w){
+                    p1 += w;
+                }
             }
         }
     }
 
 }
+
--- hw2/HW_median.cpp
+++ hw2/HW_median.cpp
@@ -4,8 +4,9 @@
 using namespace IP;
 using std::vector;
 
-extern void copyRowToBuffer(ChannelPtr<uchar>, short*, int, int); // see implementation in HW_errDiffusion.cpp
-int getMedianWithK(std::vector<int>, int);
+// from errDiff file
+extern void copyRowToBuff(ChannelPtr<uchar>, short*, int, int);
+int getMedian(std::vector<int>, int);
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // HW_median:
 //
@@ -16,84 +17,102 @@ int getMedianWithK(std::vector<int>, int);
 void
 HW_median(ImagePtr I1, int sz, ImagePtr I2)
 {
-    if(sz % 2 == 0) sz++;
+    if(sz % 2 == 0){
+        sz++;
+    }
     int k = 0;
     IP_copyImageHeader(I1, I2);
     int w = I1->width();
     int h = I1->height();
-    int total = w*h;
+    int total = w * h;
 
     int type;
     ChannelPtr<uchar> p1, p2, endd;
 
-    if (sz == 1) {
-      for(int ch = 0; IP_getChannel(I1, ch, p1, type); ch++) {
+    if(sz == 1){
+      for(int ch = 0; IP_getChannel(I1, ch, p1, type); ch++){
           IP_getChannel(I2, ch, p2, type);
-          for(endd = p1 + total; p1<endd;) *p2++ = *p1++;
+          for(endd = p1 + total; p1 < endd;){
+              *p2++ = *p1++;
+          }
       }
-    } else if (sz > 1) {
-        int bufSz = sz+w-1; // size of buffer for each padded row
+    } else if(sz > 1){
+        int buffSize = sz + w - 1; // size of buffer for each padded row
         short* buffers[sz]; // array of sz pointers
-        for (int i=0; i<sz; i++) buffers[i] = new short[bufSz];
+        for(int i = 0; i < sz; i++){
+            buffers[i] = new short[buffSize];
+        }
 
-        for(int ch = 0; IP_getChannel(I1, ch, p1, type); ch++) {
+        for(int ch = 0; IP_getChannel(I1, ch, p1, type); ch++){
             IP_getChannel(I2, ch, p2, type);
-            endd = p1+total;
+            endd = p1 + total;
 
             // copy first row to first sz/2 buffers, where are top padded rows
-            for (int i=0; i<sz/2; i++) copyRowToBuffer(p1, buffers[i], w, sz);
+            for(int i = 0; i < sz / 2; i++){
+                copyRowToBuff(p1, buffers[i], w, sz);
+            }
 
             // continue to rest of buffers, note here we still begin copying from frist row of I1
-            for (int i=sz/2; i<sz; i++) {
-                copyRowToBuffer(p1, buffers[i], w, sz);
-                p1+=w;
+            for(int i = sz / 2; i < sz; i++){
+                copyRowToBuff(p1, buffers[i], w, sz);
+                p1 += w;
             }
 
             std::vector<int> v(0);  // vector for storing neighbors
-            for (int y=0; y<h; y++) {
-                for (int i=0; i<sz; i++) {
-                    for (int j=0; j<sz; j++) {
+            for(int y = 0; y < h; y++){
+                for(int i = 0; i < sz; i++){
+                    for(int j = 0; j < sz; j++){
                         v.push_back(buffers[j][i]);
                     }
                 }
 
                 // visit each pixel in row
-                for (int x=0; x<w; x++) {
-                    *p2++ = getMedianWithK(v, k);  // use sorting to find median, k is default 0
+                for(int x = 0; x < w; x++){
+                    *p2++ = getMedian(v, k);  // use sorting to find median, k is default 0
 
-                    if (x<w-1) {
-                        v.erase(v.begin(), v.begin()+sz);  // delete outgoing column
-                        for (int i=0; i<sz; i++) {
-                            v.push_back(buffers[i][x+sz]);  // add incoming column
+                    if(x < w - 1){
+                        v.erase(v.begin(), v.begin() + sz);  // delete outgoing column
+                        for(int i = 0; i < sz; i++){
+                            v.push_back(buffers[i][x + sz]);  // add incoming column
                         }
                     }
                 }
                 v.clear();
 
-                int nextBufferIndex = (y+sz-1)%sz;
-                copyRowToBuffer(p1, buffers[nextBufferIndex], w, sz);
-                if (p1 < endd-w) p1+=w;
+                int nextBufferIndex = (y + sz - 1) % sz;
+                copyRowToBuff(p1, buffers[nextBufferIndex], w, sz);
+                if(p1 < endd - w){
+                    p1 += w;
+                }
             }
         }
-        for (int i=0; i<sz; i++) delete[] buffers[i];
+        for(int i = 0; i < sz; i++){
+            delete[] buffers[i];
+        }
     }
 }
 
 int
-getMedianWithK(std::vector<int> v, int k) {
-    int vSz = v.size();
+getMedian(std::vector<int> v, int k){
+    int vSize = v.size();
     unsigned short sum;
 
-    if (vSz==1) { return v[0]; }
-    else {
+    if(vSize == 1){
+        return v[0];
+    }
+    else{
         std::sort(v.begin(), v.end());  // sort the vector
-        int middleIndex = vSz/2;
-        if (k==0) { return v[middleIndex]; }
-        else {
-            sum=v[middleIndex];
+        int middleIndex = vSize / 2;
+        if(k == 0){
+            return v[middleIndex];
+        }
+        else{
+            sum = v[middleIndex];
             // add left k values and right k values to sum
-            for (int i=1; i<=k; i++) { sum+= (v[middleIndex-i]+v[middleIndex+i]); }
-            return sum/(k*2+1);
+            for(int i = 1; i <= k; i++){
+                sum += (v[middleIndex - i] + v[middleIndex + i]);
+            }
+            return sum / (k * 2 + 1);
         }
     }
 }
